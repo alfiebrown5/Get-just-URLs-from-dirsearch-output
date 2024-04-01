af41b7534311b951d9becbc64b3b456cf95675fe
alfiebrown5 <146943259+alfiebrown5@users.noreply.github.com>
2024-04-01 21:42:38 +0100

    Added file

---
+++ get-urls.py
@@ -0,0 +1,45 @@
+import sys
+
+#as dirsearch output is written in the format: status code, file size, url - we take the last option from each line.
+def extract_url(line):
+    parts = line.split()
+    print(parts)
+    try:
+    #gets all urls, ignoring top & empty lines of dirsearch output (command, empty line)
+        if 'http' in parts[-1]: 
+            return parts[-1]
+        else:
+            pass
+    except IndexError:
+        pass
+
+def get_urls_from_lines(lines):
+    urls = []
+    for line in lines:
+        url = extract_url(line)
+        urls.append(url)
+    #return the list without duplicates for added cleanup
+    return list(set(urls))
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <dirsearch_output_file>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            urls = get_urls_from_lines(lines)
+            with open('urls.txt', 'w') as file:
+                for url in urls:
+                    if url != None:
+                        file.write(f'{url}\n')
+            print("URLs written to urls.txt")
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+
+if __name__ == "__main__":
+    main()
+    
